d9dcd06a0fb455a7b297d0942a7605218d0ef5f0
gabriele <gabrielefesta001122@gmail.com>
2023-12-27 19:34:05 +0100

    Aggiunta classe TXTFileWriter

---
+++ unibs/ing/progettosw/utilities/TXTFileWriter.java
@@ -0,0 +1,16 @@
+package unibs.ing.progettosw.utilities;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TXTFileWriter {
+
+    public void scriviStringaSuFile(String message, String path, boolean append) throws IOException {
+        File filePath = new File(path);
+        FileWriter fw = new FileWriter(filePath,append);
+        fw.write(message);
+        fw.flush();
+        fw.close();
+    }
+}
